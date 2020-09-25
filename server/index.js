d3c8acdd2f9941695f17ac4022e897d2d7e71287
twzkraus <twzkraus@gmail.com>
2020-09-25 13:38:09 -0700

    implemented server serving static html files

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const PORT = 3001;
+const path = require('path');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '..', 'dist')));
 
 app.get('/', (req, res) => {
   console.log('GOT a GET');
