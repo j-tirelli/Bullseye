9894568cd25432cc29ac6b7f3a6c104dc0a744f4
twzkraus <twzkraus@gmail.com>
2020-09-25 15:08:27 -0700

    implemented API for retrieving products from a department

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const PORT = 3001;
+const RecommendedItem = require('../database/RecommendedItem.js');
 
 const app = express();
 
@@ -9,6 +10,14 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   console.log('GOT a GET');
 });
+
+app.get('/products/:dept', (req, res) => {
+  let formattedDept = req.params.dept[0].toUpperCase() + req.params.dept.split('').slice(1).join('');
+  RecommendedItem.find({ department: formattedDept }, (err, results) => {
+    debugger;
+    res.json(results);
+  });
+})
 
 app.listen(PORT, () => {
   console.log('running on port', PORT);
