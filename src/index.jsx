9594eef6fa57fcc4871fff2941175cb0e73722bd
twzkraus <twzkraus@gmail.com>
2020-09-29 17:26:37 -0700

    setup test env to test react components. Testing suite initialized.

--- src/index.jsx
+++ src/index.jsx
@@ -2,4 +2,5 @@ import App from './App.jsx';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app') ||
+document.createElement('div')); // create plain element for rendering--this is done for jest testing
