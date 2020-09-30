278deb91a8026722f77ba1170cdaedbf4fa61fc3
twzkraus <twzkraus@gmail.com>
2020-09-30 10:58:00 -0700

    fixed conflicts in main.js and seed.js for rebase changed example data to a module.exports array. Re-seeded db. Implemented first frontend test using jest and snapshots

--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Index from './index.jsx';
+import App from './App.jsx';
 
 test('test suite is functional', () => {
   expect(true).toBe(true);
 });
 
+test('should render at least one ListItem component', () => {
+  const component = renderer.create(
+    <App />
+  );
+  let app = component.toJSON();
+  expect(app).toMatchSnapshot();
+});
 
+/*
+test('should render at least one ListItem component', () => {});
+*/
