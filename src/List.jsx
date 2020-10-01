0f285109939d03c6f6410c0c5ae1933a24beab9a
twzkraus <twzkraus@gmail.com>
2020-10-01 15:29:28 -0700

    updates for consistency of nav buttons

--- src/List.jsx
+++ src/List.jsx
@@ -5,11 +5,11 @@ import {RecList, NavButtonRight, NavButtonLeft} from './Styles.jsx';
 const List = ({listItems, handleClick, selected}) => (
   <RecList>
     {selected > 0 ?
-    <NavButtonLeft onClick={() => handleClick(-1)}>{'<'}</NavButtonLeft> : ''}
+    <NavButtonLeft onClick={() => handleClick(-1)}>&#5176;</NavButtonLeft> : ''}
       {listItems.map(item =>
         <ListItem key={item._id} item={item}/> )}
     {selected < 3 ?
-    <NavButtonRight onClick={() => handleClick(1)}>{'>'}</NavButtonRight> : ''}
+    <NavButtonRight onClick={() => handleClick(1)}>&#5171;</NavButtonRight> : ''}
   </RecList>
 );
 
