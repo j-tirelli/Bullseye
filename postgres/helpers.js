fb226fc5fc558f2cd3381bf1aa4ebf84b5197aee
j-tirelli <john.tirelli@gmail.com>
2020-10-23 17:27:10 -0700

    Moved datagenerator file out of seed/ and into postgres/ Removed seed/ Changed script in package.json to reflect changed location Altered datagenerator as needed to fit changing needs. Returned thenables from put/post/delete methods in the helper file. Added returning keyword to end of sql statement to get changed row data. changed index.js to send back new info from postgres in the response.

--- postgres/helpers.js
+++ postgres/helpers.js
@@ -6,38 +6,39 @@ const formatName = (string) => {
 
 const getLikeDept = async (dept) => {
   let department = formatName(dept);
-  const { rows } = await db.query('SELECT * FROM item WHERE department = $1 LIMIT 100', [department])
+  const { rows } = await db.query('SELECT * FROM items WHERE department = $1 LIMIT 100', [department])
   return rows;
 };
 
 const getLikeBrands = async (brandName) => {
-  const { rows } = await db.query('SELECT * FROM item WHERE brand = $1 LIMIT 100', [brandName])
+  const { rows } = await db.query('SELECT * FROM items WHERE brand = $1 LIMIT 100', [brandName])
   return rows;
 };
 
 const getLikePrices = async (gte = 0, lte = 1000) => {
-  const { rows } = await db.query('SELECT * FROM item WHERE price <= $1 AND price >= $2', [lte, gte])
+  const { rows } = await db.query('SELECT * FROM items WHERE price <= $1 AND price >= $2', [lte, gte])
   return rows;
 };
 
 const getProduct = async (id, callback = (result) => result) => {
   id = parseInt(id);
-  const { rows } = await db.query('SELECT * FROM item WHERE id = $1', [id]);
+  const { rows } = await db.query('SELECT * FROM items WHERE id = $1', [id]);
   return rows[0];
 }
 
 const deleteProduct = async (id) => {
   id = parseInt(id);
-  await db.query('DELETE FROM item WHERE id = $1', [id]);
+  return await db.query('DELETE FROM items WHERE id = $1  RETURNING *', [id]);
+  return id;
 }
 
 const createProduct = async ({ title, brand, department, price, imageUrl, productUrl }) => {
-  const { result } = await db.query('INSERT INTO item (title, brand, department, price, imageUrl, productUrl) VALUES ($1, $2, $3, $4, $5, $6)', [title, brand, department, price, imageUrl, productUrl]);
+  return await db.query('INSERT INTO items (title, brand, department, price, "imageUrl", "productUrl") VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', [title, brand, department, price, imageUrl, productUrl]);
 }
 
 const updateProduct = async ({ id, title, brand, department, price, imageUrl, productUrl }) => {
   id = parseInt(id);
-  await db.query('UPDATE item SET title = $1, brand = $2, department = $3, price = $4, imageUrl = $5, productUrl = $6 WHERE id = $7', [title, brand, department, price, imageUrl, productUrl, id]);
+  return await db.query('UPDATE items SET title = $1, brand = $2, department = $3, price = $4, "imageUrl" = $5, "productUrl" = $6 WHERE id = $7 RETURNING *', [title, brand, department, price, imageUrl, productUrl, id]);
 }
 
 module.exports.getLikeDept = getLikeDept;
---
+++ postgres/postgresDataGenerator.js
@@ -0,0 +1,55 @@
+const faker = require('faker');
+const fs = require('fs');
+
+
+const generateRecords = function (numRecords) {
+  const brands = [];
+  const departments = [];
+  for (let i = 0; i < 20; i++) {
+    departments.push(faker.commerce.department());
+  }
+  for (let i = 0; i < 200; i++) {
+    brands.push(faker.company.companyName());
+  }
+
+  const imageSources = [
+    'http://placeimg.com/640/480/',
+    'https://loremflickr.com/320/240/'
+  ]
+
+  let writeStream = fs.createWriteStream('/home/john/example_data.csv');
+
+  writeStream.write(`title|brand|department|price|imageUrl|productUrl\n`);
+
+  const write = (offset = 1) => {
+    let ready = true;
+    while (offset <= numRecords && ready) {
+      let productName = faker.commerce.productName()
+      let productType = productName.split(' ')[2];
+      let imageUrl = imageSources[Math.floor(Math.random() * imageSources.length)] + productType + `?random=${offset}`;
+      let brand = brands[Math.floor(Math.random() * brands.length)];
+      let department = departments[Math.floor(Math.random() * departments.length)];
+      let price = Number(faker.commerce.price(0, 100)) - Math.ceil(Math.random() * 5) / 100;
+      let productUrl = offset % (numRecords + 1);
+      ready = writeStream.write(`${productName}|${brand}|${department}|${price}|${imageUrl}|/${productUrl}\n`);
+      offset++;
+    }
+    if (offset <= numRecords) {
+      writeStream.once('drain', () => {
+        write(offset);
+      });
+    } else {
+      writeStream.end();
+      console.log('stream ended');
+    }
+  }
+
+  write();
+
+};
+
+if (process.argv) {
+  let args = process.argv.slice(2);
+  let numOfRecords = args[0] || 10000000;
+  generateRecords(numOfRecords);
+}
